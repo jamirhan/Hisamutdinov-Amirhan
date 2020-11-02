65cb9fcc9a7537f7e1a8596da8fbf14ab55aa15e
jamirhan <70897571+jamirhan@users.noreply.github.com>
2020-11-02 16:05:30 +0300

    Add files via upload
    исправлено

--- Task_A.cpp
+++ Task_A.cpp
@@ -3,93 +3,128 @@
 
 // Node structure contains pointer for the
 // previous object and it's own value
+int size = 0;
+Node* up = nullptr;
+
 struct Node
 {
 	Node* prev;
 	int number;
 };
- 
- 
+
+
+void push(int value)
+{
+	// creating pointer for the element we want to push
+	Node* new_element = new Node;
+	// updating the new element fields
+	new_element->prev = up;
+	new_element->number = value;
+	// updating top element pointer
+	up = new_element;
+	std::cout << "ok" << std::endl;
+	// updating size variable since we add one new element
+	size++;
+}
+
+void pop()
+{
+	// if we don't have any top element we can't return anything
+	if (up == nullptr)
+		std::cout << "error" << std::endl;
+	else
+	{
+		// printing top value in the stack
+		std::cout << up->number << std::endl;
+		// changing top element pointer to previous
+		Node* log = up;
+		up = up->prev;
+		// freeing waste data
+		delete log;
+		// updating size value since we remove one element
+		size--;
+	}
+}
+
+
+void back()
+{
+	// if we don't have any top element we can't return anything
+	if (up == nullptr)
+		std::cout << "error" << std::endl;
+	// just printing value of the last element
+	else
+		std::cout << up->number << std::endl;
+}
+
+void get_size()
+{
+	std::cout << size << std::endl;
+}
+
+void exit()
+{
+	std::cout << "bye" << std::endl;
+	exit(0);
+}
+
+
+void clear()
+{
+	// deleting the top element while it is possible
+	while (up != nullptr)
+	{
+		// deleting the top element operation
+		Node* log = up;
+		up = up->prev;
+		// freeing waste data
+		delete log;
+	}
+	std::cout << "ok" << std::endl;
+	size = 0;
+}
+
 void stack()
 {
 	std::string command = "";// up is a name for the top object in the stack
 	Node* up = nullptr;
-    // size variable is an easy way to control
-    // number of containing elements
+	// size variable is an easy way to control
+	// number of containing elements
 	int size = 0;
-    // reading command and processing it while 
-    // the command is not "exit"
+	// reading command and processing it while 
+	// the command is not "exit"
 	while (command != "exit")
 	{
 		std::cin >> command;
-        // pushing element
+		// pushing element
 		if (command == "push")
 		{
 			int value;
 			std::cin >> value;
-            // creating pointer for the element we want to push
-			Node* new_element = new Node;
-            // updating the new element fields
-			new_element->prev = up;
-			new_element->number = value;
-            // updating top element pointer
-			up = new_element;
-			std::cout << "ok" << std::endl;
-            // updating size variable since we add one new element
-			size++;
+			push(value);
 		}
-        // removing top element
+		// removing top element
 		else if (command == "pop")
 		{
-            // if we don't have any top element we can't return anything
-			if (up == nullptr)
-				std::cout << "error" << std::endl;
-			else
-			{
-                // printing top value in the stack
-				std::cout << up->number << std::endl;
-                // changing top element pointer to previous
-				Node* log = up;
-				up = up->prev;
-                // freeing waste data
-				delete log;
-                // updating size value since we remove one element
-				size--;
-			}
+			pop();
 		}
-        // printing last element
+		// printing last element
 		else if (command == "back")
 		{
-            // if we don't have any top element we can't return anything
-			if (up == nullptr)
-				std::cout << "error" << std::endl;
-            // just printing value of the last element
-			else
-				std::cout << up->number << std::endl;
+			back();
 		}
-        // printing stack size
+		// printing stack size
 		else if (command == "size")
-			std::cout << size << std::endl;
-        // clearing the whole stack
+			get_size();
+		// clearing the whole stack
 		else if (command == "clear")
 		{
-            // deleting the top element while it is possible
-			while (up != nullptr)
-			{
-                // deleting the top element operation
-				Node* log = up;
-				up = up->prev;
-                // freeing waste data
-				delete log;
-			}
-			std::cout << "ok" << std::endl;
-			size = 0;
+			clear();
 		}
-        // exiting
+		// exiting
 		else if (command == "exit")
 		{
-			std::cout << "bye" << std::endl;
-			exit(0);
+			exit();
 		}
 	}
 }
